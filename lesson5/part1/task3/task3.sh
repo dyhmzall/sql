1b5a1c566fd8edb2cfeabd289d1c3d0a84ec6f78
dyhmzall <dyhmzall@gmail.com>
2021-07-28 16:38:25 +0300

    Домашнее задание после 5 урока. Задание 3 (исправленное)

--- lesson5/part1/task3/task3.sh
+++ lesson5/part1/task3/task3.sh
@@ -1,115 +1,68 @@
+mysql> CREATE TABLE `storehouses_products` (
+    -> id SERIAL,
+    -> storehouse_id INT UNSIGNED,
+    -> product_id INT UNSIGNED,
+    -> value INT UNSIGNED
+    -> );
+Query OK, 0 rows affected (0,01 sec)
+
+
 mysql> DESCRIBE `storehouses_products`;
-+---------------+------------------+------+-----+---------+-------+
-| Field         | Type             | Null | Key | Default | Extra |
-+---------------+------------------+------+-----+---------+-------+
-| id            | int(10) unsigned | YES  |     | NULL    |       |
-| storehouse_id | int(10) unsigned | YES  |     | NULL    |       |
-| product_id    | int(10) unsigned | YES  |     | NULL    |       |
-| value         | int(10) unsigned | YES  |     | NULL    |       |
-+---------------+------------------+------+-----+---------+-------+
-4 rows in set (0.01 sec)
++---------------+---------------------+------+-----+---------+----------------+
+| Field         | Type                | Null | Key | Default | Extra          |
++---------------+---------------------+------+-----+---------+----------------+
+| id            | bigint(20) unsigned | NO   | PRI | NULL    | auto_increment |
+| storehouse_id | int(10) unsigned    | YES  |     | NULL    |                |
+| product_id    | int(10) unsigned    | YES  |     | NULL    |                |
+| value         | int(10) unsigned    | YES  |     | NULL    |                |
++---------------+---------------------+------+-----+---------+----------------+
+4 rows in set (0,00 sec)
 
 
 mysql> SELECT * FROM `storehouses_products`;
-Empty set (0.00 sec)
+Empty set (0,01 sec)
 
 
-mysql> INSERT INTO `storehouses_products` (`value`) VALUES (ROUND(RAND()*1000));
-Query OK, 1 row affected (0.01 sec)
+mysql> INSERT INTO `storehouses_products` (`value`) VALUES (ROUND(RAND()*1000)), (0), (ROUND(RAND()*1000)), (0), (ROUND(RAND()*1000)), (0),(ROUND(RAND()*1000)), (0),(ROUND(RAND()*1000)), (0),(ROUND(RAND()*1000)), (0);
+Query OK, 12 rows affected (0,00 sec)
+Records: 12  Duplicates: 0  Warnings: 0
 
-mysql> INSERT INTO `storehouses_products` (`value`) VALUES (ROUND(RAND()*1000));
-Query OK, 1 row affected (0.01 sec)
-
-mysql> INSERT INTO `storehouses_products` (`value`) VALUES (ROUND(RAND()*1000));
-Query OK, 1 row affected (0.00 sec)
-
-mysql> INSERT INTO `storehouses_products` (`value`) VALUES (ROUND(RAND()*1000));
-Query OK, 1 row affected (0.01 sec)
-
-mysql> INSERT INTO `storehouses_products` (`value`) VALUES (ROUND(RAND()*1000));
-Query OK, 1 row affected (0.00 sec)
-
-mysql> INSERT INTO `storehouses_products` (`value`) VALUES (ROUND(RAND()*1000));
-Query OK, 1 row affected (0.00 sec)
-
-mysql> INSERT INTO `storehouses_products` (`value`) VALUES (ROUND(RAND()*1000));
-Query OK, 1 row affected (0.01 sec)
-
-mysql> INSERT INTO `storehouses_products` (`value`) VALUES (ROUND(RAND()*1000));
-Query OK, 1 row affected (0.00 sec)
-
-mysql> INSERT INTO `storehouses_products` (`value`) VALUES (ROUND(RAND()*1000));
-Query OK, 1 row affected (0.01 sec)
-
-mysql> INSERT INTO `storehouses_products` (`value`) VALUES (ROUND(RAND()*1000));
-Query OK, 1 row affected (0.01 sec)
-
-mysql> INSERT INTO `storehouses_products` (`value`) VALUES (0);
-Query OK, 1 row affected (0.00 sec)
-
-mysql> INSERT INTO `storehouses_products` (`value`) VALUES (0);
-Query OK, 1 row affected (0.00 sec)
-
-mysql> INSERT INTO `storehouses_products` (`value`) VALUES (0);
-Query OK, 1 row affected (0.01 sec)
-
-mysql> INSERT INTO `storehouses_products` (`value`) VALUES (ROUND(RAND()*1000));
-Query OK, 1 row affected (0.01 sec)
-
-mysql> INSERT INTO `storehouses_products` (`value`) VALUES (ROUND(RAND()*1000));
-Query OK, 1 row affected (0.00 sec)
-
-mysql> INSERT INTO `storehouses_products` (`value`) VALUES (0);
-Query OK, 1 row affected (0.01 sec)
-
-mysql> INSERT INTO `storehouses_products` (`value`) VALUES (0);
-Query OK, 1 row affected (0.01 sec)
 
 mysql> SELECT * FROM `storehouses_products`;
-+------+---------------+------------+-------+
-| id   | storehouse_id | product_id | value |
-+------+---------------+------------+-------+
-| NULL |          NULL |       NULL |   882 |
-| NULL |          NULL |       NULL |   232 |
-| NULL |          NULL |       NULL |   516 |
-| NULL |          NULL |       NULL |   883 |
-| NULL |          NULL |       NULL |   869 |
-| NULL |          NULL |       NULL |   693 |
-| NULL |          NULL |       NULL |   858 |
-| NULL |          NULL |       NULL |   214 |
-| NULL |          NULL |       NULL |   493 |
-| NULL |          NULL |       NULL |   826 |
-| NULL |          NULL |       NULL |     0 |
-| NULL |          NULL |       NULL |     0 |
-| NULL |          NULL |       NULL |     0 |
-| NULL |          NULL |       NULL |   649 |
-| NULL |          NULL |       NULL |   766 |
-| NULL |          NULL |       NULL |     0 |
-| NULL |          NULL |       NULL |     0 |
-+------+---------------+------------+-------+
-17 rows in set (0.00 sec)
++----+---------------+------------+-------+
+| id | storehouse_id | product_id | value |
++----+---------------+------------+-------+
+|  1 |          NULL |       NULL |   900 |
+|  2 |          NULL |       NULL |     0 |
+|  3 |          NULL |       NULL |   745 |
+|  4 |          NULL |       NULL |     0 |
+|  5 |          NULL |       NULL |    27 |
+|  6 |          NULL |       NULL |     0 |
+|  7 |          NULL |       NULL |   901 |
+|  8 |          NULL |       NULL |     0 |
+|  9 |          NULL |       NULL |   424 |
+| 10 |          NULL |       NULL |     0 |
+| 11 |          NULL |       NULL |   418 |
+| 12 |          NULL |       NULL |     0 |
++----+---------------+------------+-------+
+12 rows in set (0,00 sec)
 
 
-mysql> SELECT *, IF (`value` != 0, `value`, 1000000) as `_` FROM `storehouses_products` ORDER BY `_`;
-+------+---------------+------------+-------+---------+
-| id   | storehouse_id | product_id | value | _       |
-+------+---------------+------------+-------+---------+
-| NULL |          NULL |       NULL |   214 |     214 |
-| NULL |          NULL |       NULL |   232 |     232 |
-| NULL |          NULL |       NULL |   493 |     493 |
-| NULL |          NULL |       NULL |   516 |     516 |
-| NULL |          NULL |       NULL |   649 |     649 |
-| NULL |          NULL |       NULL |   693 |     693 |
-| NULL |          NULL |       NULL |   766 |     766 |
-| NULL |          NULL |       NULL |   826 |     826 |
-| NULL |          NULL |       NULL |   858 |     858 |
-| NULL |          NULL |       NULL |   869 |     869 |
-| NULL |          NULL |       NULL |   882 |     882 |
-| NULL |          NULL |       NULL |   883 |     883 |
-| NULL |          NULL |       NULL |     0 | 1000000 |
-| NULL |          NULL |       NULL |     0 | 1000000 |
-| NULL |          NULL |       NULL |     0 | 1000000 |
-| NULL |          NULL |       NULL |     0 | 1000000 |
-| NULL |          NULL |       NULL |     0 | 1000000 |
-+------+---------------+------------+-------+---------+
-17 rows in set (0.00 sec)
+mysql> SELECT * FROM `storehouses_products` ORDER BY IF (`value` != 0, `value`, 1000000);
++----+---------------+------------+-------+
+| id | storehouse_id | product_id | value |
++----+---------------+------------+-------+
+|  5 |          NULL |       NULL |    27 |
+| 11 |          NULL |       NULL |   418 |
+|  9 |          NULL |       NULL |   424 |
+|  3 |          NULL |       NULL |   745 |
+|  1 |          NULL |       NULL |   900 |
+|  7 |          NULL |       NULL |   901 |
+|  2 |          NULL |       NULL |     0 |
+|  4 |          NULL |       NULL |     0 |
+|  6 |          NULL |       NULL |     0 |
+|  8 |          NULL |       NULL |     0 |
+| 10 |          NULL |       NULL |     0 |
+| 12 |          NULL |       NULL |     0 |
++----+---------------+------------+-------+
+12 rows in set (0,00 sec)
