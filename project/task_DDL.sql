5ce344d15c588a414995a3ebd7fbafdb5e87dd1b
dyhmzall <dyhmzall@gmail.com>
2021-08-16 23:38:27 +0300

    Курсовой проект

---
+++ project/task_DDL.sql
@@ -0,0 +1,69 @@
+DROP DATABASE IF EXISTS `tasks`;
+CREATE DATABASE `tasks` CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `tasks`;
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users`
+(
+    `id`         SERIAL PRIMARY KEY,
+    `login`      VARCHAR(255)        NOT NULL,
+    `password`   CHAR(32)            NOT NULL,
+    `first_name` VARCHAR(255),
+    `last_name`  VARCHAR(255),
+    `email`      VARCHAR(255) UNIQUE NOT NULL
+);
+
+DROP TABLE IF EXISTS `statuses`;
+CREATE TABLE `statuses`
+(
+    `id`   SERIAL PRIMARY KEY,
+    `name` VARCHAR(255) NOT NULL
+);
+
+DROP TABLE IF EXISTS `priorities`;
+CREATE TABLE `priorities`
+(
+    `id`   SERIAL PRIMARY KEY,
+    `name` VARCHAR(255) NOT NULL
+);
+
+DROP TABLE IF EXISTS `tasks`;
+CREATE TABLE `tasks`
+(
+    `id`           SERIAL PRIMARY KEY,
+    `user_id`      BIGINT UNSIGNED NOT NULL,
+    `status_id`    BIGINT UNSIGNED NOT NULL,
+    `priority_id`  BIGINT UNSIGNED NOT NULL,
+    `name`         VARCHAR(255),
+    `description`  TEXT,
+    `url`          VARCHAR(255) COMMENT 'ссылка на задачу в сети',
+    `created_at`   DATETIME DEFAULT CURRENT_TIMESTAMP,
+    `updated_at`   DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `deadline_at`  DATETIME,
+    `planned_time` INT COMMENT 'запланированное время',
+
+    FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+    FOREIGN KEY (`status_id`) REFERENCES `statuses` (`id`),
+    FOREIGN KEY (`priority_id`) REFERENCES `priorities` (`id`)
+);
+
+DROP TABLE IF EXISTS `lists`;
+CREATE TABLE `lists`
+(
+    `id`      SERIAL PRIMARY KEY,
+    `user_id` BIGINT UNSIGNED NOT NULL,
+    `name`    VARCHAR(255),
+
+    FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+);
+
+DROP TABLE IF EXISTS `lists_tasks`;
+CREATE TABLE `lists_tasks`
+(
+    `list_id` BIGINT UNSIGNED NOT NULL,
+    `task_id` BIGINT UNSIGNED NOT NULL,
+
+    PRIMARY KEY (`list_id`, `task_id`),
+    FOREIGN KEY (`list_id`) REFERENCES `lists` (`id`),
+    FOREIGN KEY (`task_id`) REFERENCES `tasks` (`id`)
+);
